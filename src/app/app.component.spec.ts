40edf019a8f8c7ba7e70c9f6f3365300d35ccc27
Simone Ramundi <simoneramundi@Mac.station>
2025-10-18 19:33:41 +0200

    Fixed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,10 +14,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hello' title`, () => {
+  it(`should have the welcoming title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello');
+    expect(app.title).toBeTruthy;
   });
 
   it('should render title', () => {
